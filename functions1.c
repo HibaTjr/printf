06c32e3447b62127b785c05b24b1e502e083cb8d
HibaTjr <tajerhiba1@gmail.com>
2023-09-18 07:40:32 -0700

    printf_project

---
+++ functions1.c
@@ -0,0 +1,74 @@
+#include "main.h"
+/************************* PRINT UNSIGNED NUMBER *************************/
+/**
+* print_unsigned - Prints an unsigned number
+* @types: List a of arguments
+* @buffer: Buffer array to handle print
+* @flags:  Calculates active flags
+ * @width: get width
+* @precision: Precision specification
+* @size: Size specifier 
+* Return: Number of chars printed.
+*/
+int print_unsigned(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+int i = BUFF_SIZE - 2;
+unsigned long int num = va_arg(types, unsigned long int);
+num = convert_size_unsgnd(num, size);
+if (num == 0)
+buffer[i--] = '0';
+buffer[BUFF_SIZE - 1] = '\0';
+for (; num > 0; num /= 10)
+{
+buffer[i--] = (num % 10) + '0';
+}
+i++;
+return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+/************* PRINT UNSIGNED NUMBER IN OCTAL  ****************/
+/**
+* print_octal - Prints an unsigned number in octal notation
+* @types: Lista of arguments
+* @buffer: Buffer array to handle print
+* @flags:  Calculates active flags
+* @width: get width
+* @precision: Precision specification
+* @size: Size specifier
+* Return: Number of chars printed
+*/
+int print_octal(va_list types, char buffer[],
+int flags, int width, int precision, int size)
+{
+int i = BUFF_SIZE - 2;
+unsigned long int num = va_arg(types, unsigned long int);
+unsigned long int init_num = num;
+UNUSED(width);
+num = convert_size_unsgnd(num, size);
+if (num == 0)
+{
+buffer[i--] = '0';
+}
+buffer[BUFF_SIZE - 1] = '\0';
+do
+{
+    buffer[i--] = (num % 8) + '0';
+    num /= 8;
+} while (num > 0);
+if (flags & F_HASH && init_num != 0)
+buffer[i--] = '0';
+i++;
+return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
+do
+{
+buffer[i--] = map_to[num % 16];
+num /= 16;
+} while (num > 0);
+if (flags & F_HASH && init_num != 0)
+{
+buffer[i--] = flag_ch;
+buffer[i--] = '0';
+}
+i++;
+return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+}
